e3eb50ab33d0103e4a3cd39d020816651df85564
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-08-14 09:57:46 +0530

    Shadow

--- Shadow/__main__.py
+++ Shadow/__main__.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import pyrogram
 import asyncio
 import os
 from importlib import import_module
@@ -20,48 +21,21 @@ from importlib import import_module
 from aiogram import executor
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
-from Shadow import TOKEN, bot, dp
+from Shadow import TOKEN
 from Shadow.config import get_bool_key, get_list_key
 from Shadow.modules import ALL_MODULES, LOADED_MODULES, MOD_HELP
+from Shadow.services.pyrogram import pbot
 from Shadow.utils.logger import log
 
 if get_bool_key("DEBUG_MODE"):
     log.debug("Enabling logging middleware.")
-    dp.middleware.setup(LoggingMiddleware())
 
 LOAD = get_list_key("LOAD")
 DONT_LOAD = get_list_key("DONT_LOAD")
 
-if get_bool_key("LOAD_MODULES"):
-    if len(LOAD) > 0:
-        modules = LOAD
-    else:
-        modules = ALL_MODULES
 
-    modules = [x for x in modules if x not in DONT_LOAD]
-
-    log.info("Modules to load: %s", str(modules))
-    for module_name in modules:
-        # Load pm_menu at last
-        if module_name == "pm_menu":
-            continue
-        log.debug(f"Importing <d><n>{module_name}</></>")
-        imported_module = import_module("Shadow.modules." + module_name)
-        if hasattr(imported_module, "__help__"):
-            if hasattr(imported_module, "__mod_name__"):
-                MOD_HELP[imported_module.__mod_name__] = imported_module.__help__
-            else:
-                MOD_HELP[imported_module.__name__] = imported_module.__help__
-        LOADED_MODULES.append(imported_module)
-    log.info("Modules loaded!")
-else:
-    log.warning("Not importing modules!")
-
-loop = asyncio.get_event_loop()
-
-import_module("Shadow.modules.pm_menu")
 # Import misc stuff
-import_module("Shadow.utils.exit_gracefully")
+
 if not get_bool_key("DEBUG_MODE"):
     import_module("Shadow.utils.sentry")
 
@@ -72,34 +46,55 @@ async def before_srv_task(loop):
         loop.create_task(module.__before_serving__(loop))
 
 
-async def start(_):
+async def start():
     log.debug("Starting before serving task for all modules...")
     loop.create_task(before_srv_task(loop))
-
+    await pbot.start()
     if not get_bool_key("DEBUG_MODE"):
         log.debug("Waiting 2 seconds...")
         await asyncio.sleep(2)
 
 
 async def start_webhooks(_):
-    url = os.getenv("WEBHOOK_URL") + f"/{TOKEN}"
-    await bot.set_webhook(url)
+    os.getenv("WEBHOOK_URL") + f"/{TOKEN}"
     return await start(_)
 
 
 log.info("Starting loop..")
-log.info("Aiogram: Using polling method")
+log.info("Pyrogram: Using polling method")
 
-if os.getenv("WEBHOOKS", False):
-    port = os.getenv("WEBHOOKS_PORT", 8080)
-    executor.start_webhook(dp, f"/{TOKEN}", on_startup=start_webhooks, port=port)
-else:
-    executor.start_polling(
-        dp,
-        loop=loop,
-        on_startup=start,
-        timeout=15,
-        relax=0.1,
-        fast=True,
-        skip_updates=True,
-    )
+
+async def run_bot():
+    """Run The Bot"""
+    await pbot.start()
+    if get_bool_key("LOAD_MODULES"):
+        if len(LOAD) > 0:
+            modules = LOAD
+        else:
+            modules = ALL_MODULES
+
+        modules = [x for x in modules if x not in DONT_LOAD]
+
+        log.info("Modules to load: %s", str(modules))
+        for module_name in modules:
+            # Load pm_menu at last
+            if module_name == "pm_menu":
+                continue
+            log.debug(f"Importing <d><n>{module_name}</></>")
+            imported_module = import_module("Shadow.modules." + module_name)
+            if hasattr(imported_module, "__help__"):
+                if hasattr(imported_module, "__mod_name__"):
+                    MOD_HELP[imported_module.__mod_name__] = imported_module.__help__
+                else:
+                    MOD_HELP[imported_module.__name__] = imported_module.__help__
+            LOADED_MODULES.append(imported_module)
+        log.info("Modules loaded!")
+    else:
+        log.warning("Not importing modules!")
+
+    log.info("Here We Go")
+    await pyrogram.idle()
+
+
+if __name__ == "__main__":
+    pbot.loop.run_until_complete(run_bot())
