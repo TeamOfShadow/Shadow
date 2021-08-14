568943b5b620bb1ed53d2df1dc377eca23cca3b7
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-08-14 12:44:51 +0530

    Shadow

--- Shadow/services/telethon.py
+++ Shadow/services/telethon.py
@@ -21,6 +21,8 @@ from Shadow.config import get_int_key, get_str_key
 TOKEN = get_str_key("TOKEN", required=True)
 NAME = TOKEN.split(":")[0]
 
+version = "1.3.20"
+
 tbot = TelegramClient(
     NAME, get_int_key("APP_ID", required=True), get_str_key("APP_HASH", required=True)
 )
