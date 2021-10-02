98e3e50db6de5af2e2579c88257e1096ee6c0598
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-10-02 15:42:28 +0530

    Update Cricket_Score.py

--- Shadow/modules/Cricket_Score.py
+++ Shadow/modules/Cricket_Score.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-import urllib.request 
+import urllib.request
 
 from bs4 import BeautifulSoup
 from telethon import events
