9f87843c1f9d093a83962cd18b31b26e0c1b2acf
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-06 19:07:06 +0530

    Lol

--- Shadow/modules/pm_menu.py
+++ Shadow/modules/pm_menu.py
@@ -19,6 +19,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import random
+
 from contextlib import suppress
 
 from aiogram.types.inline_keyboard import InlineKeyboardButton, InlineKeyboardMarkup
