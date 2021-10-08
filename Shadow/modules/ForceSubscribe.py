67ad3f35ab43addd2dc1edad907a39c1df888c01
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-08 19:39:29 +0530

    Shadow

--- Shadow/modules/ForceSubscribe.py
+++ Shadow/modules/ForceSubscribe.py
@@ -195,21 +195,21 @@ def config(client, message):
 
 
 __help__ = """
-**ForceSubscribe:**
+<b>ForceSubscribe</b>
 - Shadow can mute members who are not subscribed your channel until they subscribe
 - When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 
-**Setup**
+<b>Setup</b>
 1) First of all add me in the group as admin with ban users permission and in the channel as admin.
 
-**Note:** Only creator of the group can setup me and I will not allow force subscribe again if not done so.
+<b>Note:</b> Only creator of the group can setup me and I will not allow force subscribe again if not done so.
  
-**Commmands**
- - `/forcesubscribe` - To get the current settings.
- - `/forcesubscribe <no/off/disable>` - To turn off ForceSubscribe.
- - `/forcesubscribe {channel username}` - To turn on and setup the channel.
- - `/forcesubscribe clear` - To unmute all members who muted by me.
+<b>Commmands</b>
+ - /forcesubscribe: To get the current settings.
+ - /forcesubscribe <code>[no/off/disable]</code>: To turn off ForceSubscribe.
+ - /forcesubscribe <code>[channel username]</code>: To turn on and setup the channel.
+ - /forcesubscribe <code>clear</code>: To unmute all members who muted by me.
  
-**Note:** /forcesub is an alias of /forcesubscribe
+<b>Note:</b> <code>/forcesub</code> is an alias of <code>/forcesubscribe</code>
 """
-__mod_name__ = "Force Subscribe "
+__mod_name__ = "Force Subscribe"
