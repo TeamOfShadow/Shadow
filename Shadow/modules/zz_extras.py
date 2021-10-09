7ebafe3f9d690ff01a60c895e985de6853aae37c
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-09 07:00:41 +0530

    Shadow

--- Shadow/modules/zz_extras.py
+++ Shadow/modules/zz_extras.py
@@ -18,57 +18,57 @@ __mod_name__ = "🛠 Extras"
 __help__ = """
 The module that contains extra tools that help you to do many cool stuff.
 
-<b> Available Commands </b>
+<b>Available Commands:</b>
 
 <b>AFK</b>
-- `/afk <reason>` : mark yourself as AFK (Away From Keyboard)
+- /afk <reason> : Mark yourself as AFK (Away From Keyboard)
   
 <b>URL LOCK</b> 
  Block links sent by users in your group 
-- `/urllock [on/off]`: Enable/Disable URL Lock
+- /urllock [on/off]: Enable/Disable URL Lock
 
 <b>DATE TIME</b>
-- `/datetime [timezone]`: Get the present date and time information
+- /datetime [timezone]: Get the present date and time information
 You can check out this [link](https://timezonedb.com/time-zones) for the available timezones
 
 <b>PASTE</b>
-- `/paste [reply]`
+- /paste [reply]
 Usage: Create a paste or a shortened url using [nekobin](https://nekobin.com)
 
 <b>TELEGRAPH</b>
-- `/telegraph media`: To Make Link of Any Image Or MP4 video. 
-- `/telegraph text`: To make Link of Any Text Written. 
+- /telegraph media: To Make Link of Any Image Or MP4 video. 
+- /telegraph text: To make Link of Any Text Written. 
 
 <b>TORRENT</b>
-- `/torrent [QUERY]`: Search for torrent links
+- /torrent [QUERY]: Search for torrent links
 
 <b>TEXT TO SPEECH & SPEECH TO TEXT</b>
-- `/tts`: Reply to any message to get text to speech output
-- `/stt`: Type in reply to a voice message (english only) to extract text from it.
+- /tts: Reply to any message to get text to speech output
+- /stt: Type in reply to a voice message (english only) to extract text from it.
 
 <b>VIRUS SCAN</b>
-- `/scanit`: Scan a file for virus (MAX SIZE = 3MB)
+- /scanit: Scan a file for virus (MAX SIZE = 3MB)
  
 <b>COUNTRY</b>
-- `/country [country name]`: Gathering info about given country
+- /country [country name]: Gathering info about given country
 
 <b>COVID</b>              
-- `/covid`: To Get Global Stats of Covid.
-- `/covid [COUNTRY]`: To Get Stats of A Single Country.
+- /covid: To Get Global Stats of Covid.
+- /covid [COUNTRY]: To Get Stats of A Single Country.
   
 <b>CRICKET INFO</b>     
-- `/cs`: Gathers cricket information (globally)
+- /cs: Gathers cricket information (globally)
 
 <b>QR CODE</b>     
-- `/getqr`: Get text in qr.
-- `/makeqr`: Make a qr code.
+- /getqr: Get text in qr.
+- /makeqr: Make a qr code.
   
 <b> KARMA </b>
 [UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
 [DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
 
-- `/karma [ON/OFF]`: Enable/Disable karma in group. 
-- `/karma [Reply to a message]`: Check user's karma
-- `/karma`: Chek karma list of top 10 users
+- /karma [ON/OFF]: Enable/Disable karma in group. 
+- /karma [Reply to a message]: Check user's karma
+- /karma: Chek karma list of top 10 users
         
 """
