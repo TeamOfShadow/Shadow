c90aeadbfb50cd7111ea307a77a0e773db58c8e8
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-10 09:25:07 +0530

    Shadow

--- Shadow/modules/error.py
+++ Shadow/modules/error.py
@@ -114,9 +114,10 @@ async def all_errors_handler(update: Update, error):
 
     text = "<b>Sorry, I found a error!</b>\n\n"
     text += "<b>Forward this to @ShadowSupport_Official</b>\n\n"
-    text += "<i>-----------Starting Crash Log------------<i>\n"
+    text += "<i>-----------Starting Crash Log------------</i>\n"
     text += f"<code>{html.escape(err_tlt, quote=False)}: {html.escape(err_msg, quote=False)}</code>\n"
-    text += "<i>-----------Finishing Crash Log-----------</i>"
+    text += "<i>-----------Finishing Crash Log-----------</i>\n\n"
+    text += "© 2020-2021 @TeamOfShadow"
     redis.set(chat_id, str(error), ex=600)
     await bot.send_message(chat_id, text)
 
