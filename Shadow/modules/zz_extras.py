c47596cf1d28bfb24bc0cb4ac9ea9ef3550cc0f6
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-10 11:59:23 +0530

    Shadow

--- Shadow/modules/zz_extras.py
+++ Shadow/modules/zz_extras.py
@@ -15,9 +15,8 @@
 
 __mod_name__ = "🛠 Extras"
 
-__help__ = [
-    ".",
-    """
+__help__ = (
+"""
 The module that contains extra tools that help you to do many cool stuff.
 
 <b>Available Commands:</b>
@@ -73,4 +72,4 @@ Usage: Create a paste or a shortened url using [nekobin](https://nekobin.com)
 - /karma <code>[Reply to a message]</code>: Check user's karma
 - /karma: Chek karma list of top 10 users
 """,
-]
+)
