8b4dfdbfc46d9613ad218e9c7665bc1d6a395911
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-10 11:37:06 +0530

    Shadow

--- Shadow/modules/zz_misc.py
+++ Shadow/modules/zz_misc.py
@@ -15,7 +15,8 @@
 
 __mod_name__ = "Misc ⚙️"
 
-__help__ = """
+__help__ = (
+"""
 An 'odds and ends' module for small, simple commands which don't really fit anywhere.
 
 <b>Available commands:</b>
@@ -75,3 +76,4 @@ Example syntax: <code>/cash 1 USD LKR</code>
 - /ip <code>(url)<code>: Displays information about an IP / domain.
 - /direct <code>(url)<code>: Generates direct links from the sourceforge.net
 """
+)
