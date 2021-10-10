e05d2b71d4bce3be1c349174e70c6a00996da0af
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-10 11:54:44 +0530

    Shadow

--- Shadow/modules/covid.py
+++ Shadow/modules/covid.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+
 from pyrogram import filters
 
 from Shadow.function.pluginhelpers import fetch, json_prettify
