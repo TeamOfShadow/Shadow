7d6ebc4750ab9c6e9ae9a877c910a4990c82e79d
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-16 16:51:27 +0530

    Shadow

--- Shadow/modules/pm_menu.py
+++ Shadow/modules/pm_menu.py
@@ -78,7 +78,7 @@ async def get_start_func(message, strings, edit=False):
     buttons.add(
         InlineKeyboardButton(strings["btn_lang"], callback_data="lang_btn"),
         InlineKeyboardButton(
-            strings["btn_source"], url="https://github.com/TeamOfShadow/Shadow"
+            strings["btn_source"], url="https://github.com/TeamOfShadow/"
         ),
     )
     buttons.add(
