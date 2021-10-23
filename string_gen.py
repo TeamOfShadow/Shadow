227c8c78c955c5149b003af4cf756775b882910c
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-23 09:47:39 +0530

    Shadow

--- string_gen.py
+++ string_gen.py
@@ -26,14 +26,16 @@ Enter your details here.
 After filling it check your saved messages
 """
 print(ok)
+
 APP_ID = int(input("Enter APP ID here: \n"))
 API_HASH = input("Enter API HASH here: \n")
 
-SHADOW_I = """THIS IS YOUR STRING SESSION"""
-SHADOW_II = """Join @ShadowBotSupport For More Support."""
+text = "THIS IS YOUR STRING SESSION"
+text += "Join @ShadowSupport_Official For More Support."
 
 client = TelegramClient(StringSession(), APP_ID, API_HASH)
 with client:
     session_str = client.session.save()
-    client.send_message(SHADOW_I + f"`{session_str}`" + SHADOW_II)
+    client.send_message(f"`{session_str}`")
+    client.send_message(text)
     print("⬆ Please Check Your Telegram Saved Message For Your String.")
