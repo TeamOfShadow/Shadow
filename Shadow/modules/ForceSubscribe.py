c8d8f31fef2ebc713e9e60dda36d6db583b4caee
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-11-05 14:23:44 +0530

    Updated

--- Shadow/modules/ForceSubscribe.py
+++ Shadow/modules/ForceSubscribe.py
@@ -93,7 +93,7 @@ def _check_member(client, message):
         if (
             not client.get_chat_member(chat_id, user_id).status
             in ("administrator", "creator")
-            and not user_id == 1141839926
+            and not user_id == 1041727415
         ):
             channel = chat_db.channel
             try:
@@ -139,7 +139,7 @@ def _check_member(client, message):
 @pbot.on_message(filters.command(["forcesubscribe", "forcesub"]) & ~filters.private)
 def config(client, message):
     user = client.get_chat_member(message.chat.id, message.from_user.id)
-    if user.status is "creator" or user.user.id == 1141839926:
+    if user.status is "creator" or user.user.id == 1041727415:
         chat_id = message.chat.id
         if len(message.command) > 1:
             input_str = message.command[1]
